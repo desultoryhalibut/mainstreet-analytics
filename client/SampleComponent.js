8f23db0b6049fe0ffa4db22afd3777ee58855a68
Tee Blagdon <tjlay23@gmail.com>
2016-07-14 12:12:47 -0700

    Feat: Create front-end file system

--- client/SampleComponent.js
+++ client/SampleComponent.js
@@ -1,13 +1,13 @@
 import React from 'react';
- 
+
 class SampleComponent extends React.Component {
   constructor() {
     super();
     this.state = {
-  
+
     };
   }
- 
+
   render() {
     return (
       <div>

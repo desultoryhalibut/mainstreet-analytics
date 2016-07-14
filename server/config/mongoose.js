c0405d85858b946715a188152af38cbbb4d7cacc
Tee Blagdon <tjlay23@gmail.com>
2016-07-14 15:58:08 -0700

    Feat: Update mongodb path on Digital Ocean

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 module.exports = function() {
 
-  mongoose.connect('mongodb://localhost/users');
+  // DigitalOcean MongoDB droplet
+  mongoose.connect('mongodb://localhost:27017/mainstreet');
 
   const db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'Error connecting to database'));
   db.once('open', console.log.bind(console, 'Connected to database!'));
-
 
 };

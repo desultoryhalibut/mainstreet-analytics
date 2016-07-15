16fe3f8c0b354189345afd0f085e4442bd3a145b
Christine Chou <christinejchou@gmail.com>
2016-07-15 14:47:55 -0700

    Set up schemas for database

---
+++ server/sentiment/news-sentiment-model.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var nSentimentSchema = new Schema({
+  newsTopic: {type: String},
+  relevance: {type: Number},
+  sentimentScore: {type: String},
+  type: {type: String},
+})
+
+nSentimentSchema.set('timestamps', true);
+
+var NSentiment = mongoose.model('NSentiment', nSentimentSchema);
+
+module.exports = NSentiment;

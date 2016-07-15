2f9c978092e3fd6c5606c755104ab0b9abb6d3cd
Tee Blagdon <tjlay23@gmail.com>
2016-07-14 17:16:29 -0700

    Feat: Add tests for API requests

---
+++ test/test.js
@@ -0,0 +1,69 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const request = require('request');
+
+const app = require('../server/server');
+const db = require('../server/config/mongoose');
+// insert models here
+
+
+// TO DO: edit tests once database and routing is set up
+
+describe('GET /api/users', function() {
+  it('should list ALL users on /api/users GET', function(done) {
+    chai.request(app)
+      .get('/users')
+      .end(function(err, res){
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
+
+describe('API Tests', function() {
+  describe ('GET /api/twitter', function() {
+    it('should list ALL stored Tweets on /api/twitter GET', function(done) {
+      chai.request(app)
+        .get('/api/twitter')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe ('GET /api/sentiment', function() {
+    it('should list ALL Sentiments on /api/sentiment GET', function(done) {
+      chai.request(app)
+        .get('/api/sentiment')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+
+  describe ('GET /api/finance', function() {
+    it('should list ALL finance data on /api/finance GET', function(done) {
+      chai.request(app)
+        .get('/api/news')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe ('GET /api/news', function() {
+    it('should list ALL news data on /api/news GET', function(done) {
+      chai.request(app)
+        .get('/api/news')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+});

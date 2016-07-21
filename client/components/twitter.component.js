7e26ab45165e0b79a38d9d18b100e8a0e9c2d5d1
Quin Kinser <quinkinser@gmail.com>
2016-07-21 16:11:40 -0700

    Remove private twitter keys
    Twitter controller still does not work with process.env variables.

--- client/components/twitter.component.js
+++ client/components/twitter.component.js
@@ -8,27 +8,7 @@ class TwitterChart extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h2>Twitter Chart</h2>
-        <VictoryChart />
-      </div>
-    	<VictoryChart>
-    	  <VictoryLine
-    	    style={{data:
-    	      {stroke: "red", strokeWidth: 4}
-    	    }}
-    	    y={(data) =>
-    	      Math.sin(2 * Math.PI * data.x)
-    	    }
-    	  />
-    	  <VictoryLine
-    	    style={{data:
-    	      {stroke: "blue", strokeWidth: 4}
-    	    }}
-    	    y={(data) => Math.cos(2 * Math.PI * data.x)}
-    	  />
-    );
+    
   }
 }
 

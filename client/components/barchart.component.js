2698534c2e5801185fc9d5b7536cfd4e542d418a
Christine Chou <christinejchou@gmail.com>
2016-07-23 14:10:38 -0700

    Refactor sentiment barchart

--- client/components/barchart.component.js
+++ client/components/barchart.component.js
@@ -17,12 +17,63 @@ class CentralAxis extends Component {
     return data;
   }
 
-
   render() {
     var length = this.props.data.length;
 
     return (
 
+      // <div className='bar-chart'>
+      <svg width={500} height={350}>
+        <VictoryChart horizontal
+          height={350}
+          width={500}
+          padding={40}
+          domain={{x:[-1.2,1.2], y:[0,7]}}
+          style={{
+            data: {width: 50},
+            labels: {fontSize: 12}
+          }}
+          padding={{
+            top: 20,
+            bottom: 60,
+            left: 20,
+            right: 20
+          }}
+          domainPadding={{x: 15}}>
+          <VictoryAxis
+            label="Sentiment"
+            orientation="bottom"/>
+          <VictoryAxis 
+            domain={{y:[0, 5]}}
+            tickValues={[-1, -0.75, -0.5, -.25, 0, .25, .50, .75, 1]}
+            style={{
+              labels: {fontSize: 9},
+              grid: {
+                stroke: "grey",
+                strokeWidth: 1
+              },
+              axis: {stroke: "transparent"},
+              ticks: {stroke: "transparent"}
+            }}/>
+          <VictoryBar horizontal
+            style={{
+              data: {
+                width: 17,
+                labels: { padding: 5, fontSize:10 },
+                fill: (data) => data.y > 0 ?
+                  "gold" : "blue"
+              }
+            }}
+            data={this.props.data.map(function(obj, idx) {
+                
+              return {
+                x: 1+idx,
+                y: + obj.sentimentScore,
+                label: obj.newsTopic.toUpperCase()
+              }
+            })}
+          />   
+        </VictoryChart>
       <div className='bar-chart'>
       <svg width={600} height={400}>
       <VictoryChart
@@ -83,8 +134,9 @@ class CentralAxis extends Component {
           />
           </VictoryChart>
       </svg>
-      </div>
 
+
+      // </div>
     );
   }
 }

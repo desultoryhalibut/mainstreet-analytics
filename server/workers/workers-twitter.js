24a7e7082bdd3b0c4ffc89420ced25471b5ad81d
Carlos Espinal <cespinalt@gmail.com>
2016-07-23 16:21:44 -0700

    Implement worker for twitter

---
+++ server/workers/workers-twitter.js
@@ -0,0 +1,73 @@
+const twitterModels = require('../twitter/twitter-model');
+const request = require('request');
+const mongoose = require('mongoose');
+var uri = 'mongodb://QuinKinser:Ron1680.@ds031608.mlab.com:31608/leaderboards';
+db = mongoose.connect(uri);
+
+const interval = 60000;
+const previousDate = Date.now() - interval;
+
+const channels = {
+  'nintendo' : ['nintendo'],
+  'google' : ['google'],
+  'ford': ['ford'],
+  'disney': ['disney'],
+  'genentech': ['genentech'],
+  'gold': ['gold'],
+  'negative': ['sell', 'volatility', 'panic', 'hedge'],
+  'markets': ['dow', 's&p', 'stocks'],
+};
+
+
+const averageTweets = function(tweets, topic) {
+  var tweets;
+  const average = tweets.reduce(function(a, b) {
+    return a + b.sentiment;
+  }, 0);
+
+  const numTweets = tweets.length;
+  const tgroupAverage = {
+    numTweets: numTweets,
+    sentimentAverage: average / numTweets,
+    time: Date.now()
+  };
+
+  twitterModels.TweetAverage.find({keyword: topic}, function(err, t) {
+    if(!err) {
+      if(t.length > 0) {
+        twitterModels.TweetAverage.update({keyword: topic}, {$push: { data: tgroupAverage }}, function(err) {
+          if(!err) {
+            console.log('Record updated');
+          }
+        });
+      } else {
+        twitterModels.TweetAverage.create({keyword: topic, data: [' ']}, function (err) {
+        });
+      }
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+
+var findTweet = function(topic){
+
+  twitterModels.Tweet.find({tag: topic, time: {$gte: previousDate}}, function(err, tweets) {
+    if(err) {
+      console.log(err);
+    } else {
+      averageTweets(tweets, topic);
+    }
+  });
+};
+
+
+
+const getCollections = function(channels) {
+  for(var topic in channels) {
+    findTweet(topic);
+  }
+};
+
+getCollections(channels);

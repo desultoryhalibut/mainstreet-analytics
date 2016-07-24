914385ef75edacc135e01aaa7cdeaa0841931b78
Quin Kinser <quinkinser@gmail.com>
2016-07-24 07:07:39 -0700

    Add correct labels to twitter summary bar chart

--- client/components/twitter-live-summary.component.js
+++ client/components/twitter-live-summary.component.js
@@ -27,10 +27,12 @@ class TwitterLiveSummary extends Component {
     }
 
     var intervals = this.state.intervals;
-    var yVals = this.props.twitterData.map(function(obj, index, collection){
-      return (obj.data.slice( intervals ).reduce(function(a, b){
-               return a + b.numTweets;
-             }, 0) / -intervals); //find avg volume for 'this.state.test' number of intervals
+    var data = this.props.twitterData.map(function(obj, index, collection){
+      return {x: index + 1,
+              y: (obj.data.slice( intervals ).reduce(function(a, b){
+                    return a + b.numTweets;
+                  }, 0) / -intervals),
+              label: obj.keyword}; //find avg volume for 'this.state.test' number of intervals
     })
 
     var chart = <VictoryBar
@@ -40,14 +42,14 @@ class TwitterLiveSummary extends Component {
         labels: {fontSize: 10}
       }}
       data={[
-        {x: 1, y: yVals[0], fill: "gold", label: "query1"},
-        {x: 2, y: yVals[1], fill: "orange", label: "query2"},
-        {x: 3, y: yVals[2], fill: "tomato", label: "query3"},
-        {x: 4, y: yVals[3], fill: "pink", label: "query4"},
-        {x: 5, y: yVals[4], fill: "magenta", label: "query5"},
-        {x: 6, y: yVals[5], fill: "purple", label: "query6"},
-        {x: 7, y: yVals[6], fill: "blue", label: "query7"},
-        {x: 8, y: yVals[7], fill: "teal", label: "query8"}
+        {x: 1, y: data[0].y, fill: "gold", label: data[0].label},
+        {x: 2, y: data[1].y, fill: "orange", label: data[1].label},
+        {x: 3, y: data[2].y, fill: "tomato", label: data[2].label},
+        {x: 4, y: data[3].y, fill: "pink", label: data[3].label},
+        {x: 5, y: data[4].y, fill: "magenta", label: data[4].label},
+        {x: 6, y: data[5].y, fill: "purple", label: data[5].label},
+        {x: 7, y: data[6].y, fill: "blue", label: data[6].label},
+        {x: 8, y: data[7].y, fill: "teal", label: data[7].label}
       ]}
     />
 
